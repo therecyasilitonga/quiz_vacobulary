827966b5020979f52c6b686659d5252bee5b0e41
therecyasilitonga <therecyasilitonga@student.uir.ac.id>
2025-12-12 03:47:56 +0700

    first commit

--- game/vite.config.js
+++ game/vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
-    quasar({
-      sassVariables: 'src/quasar-variables.sass'
-    })
-  ],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    port: 5173,
-    open: true
-  }
+  plugins: [vue()],
 })
